b3ab43b8c9b81965bd091e9154a6f6f85507157b
joserobjr <jose.rob.jr@gmail.com>
2017-05-26 01:56:25 -0300

    Moves the immutable collections to a new project

---
+++ src/test/kotlin/tests.kt
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (C) 2017 José Roberto de Araújo Júnior
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ *  Copyright (C) 2017 José Roberto de Araújo Júnior
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import io.kotlintest.specs.ShouldSpec
+import org.platestack.structure.immutable.immutableMapOf
+
+class Maps: ShouldSpec() {
+    init {
+        val empty = immutableMapOf<String, String>()
+        val single = immutableMapOf("a" to "b")
+        val multiple = immutableMapOf("a" to "b", "b" to "c", "c" to "d")
+    }
+}
